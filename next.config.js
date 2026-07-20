465888c90c611dec9fb430c5f73003c17ef9b179
osher <osherp96@gmail.com>
2026-07-20 12:27:00 +0300

    fix: proxy structure

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const nextConfig = {
         source: '/api-backend/:path*',
         destination: 'https://site-monitor-backend.onrender.com/:path*',
       },
-      {
-        source: '/api-backend',
-        destination: 'https://site-monitor-backend.onrender.com/',
-      }
     ];
   },
 };
